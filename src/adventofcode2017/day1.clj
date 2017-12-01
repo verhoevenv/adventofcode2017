b4ca048174af12c693626c32b17591c87a72ee00
Vincent Verhoeven <verhoevenv@gmail.com>
2017-12-01 23:57:33 +0100

    Day 1 part 2

--- src/adventofcode2017/day1.clj
+++ src/adventofcode2017/day1.clj
@@ -4,13 +4,23 @@
   [d]
   (Character/digit d 10))
 
+(defn rotate
+  [n s]
+  (concat (drop n s) (take n s)))
+
 (defn val-if-matches
   [x y]
   (if (= x y) x 0))
   
-(defn captcha
+(defn captcha-next
   "Solves a not-human captcha"
   [captcha-string]
   (let [digits (map digit captcha-string)]
-    (let [next-digits (concat (rest digits) [(first digits)])]
+    (let [next-digits (rotate 1 digits)]
+      (reduce + (map val-if-matches digits next-digits)))))
+
+(defn captcha-half
+  [captcha-string]
+  (let [digits (map digit captcha-string)]
+    (let [next-digits (rotate (/ (count digits) 2) digits)]
       (reduce + (map val-if-matches digits next-digits)))))
--- test/adventofcode2017/day1_test.clj
+++ test/adventofcode2017/day1_test.clj
@@ -3,7 +3,15 @@
             [adventofcode2017.day1 :refer :all]))
 
 (deftest part-1
-  (is (= 3 (captcha "1122")))
-  (is (= 4 (captcha "1111")))
-  (is (= 0 (captcha "1234")))
-  (is (= 9 (captcha "91212129"))))
+  (is (= 3 (captcha-next "1122")))
+  (is (= 4 (captcha-next "1111")))
+  (is (= 0 (captcha-next "1234")))
+  (is (= 9 (captcha-next "91212129"))))
+
+(deftest part-2
+  (is (= 6  (captcha-half "1212")))
+  (is (= 0  (captcha-half "1221")))
+  (is (= 4  (captcha-half "123425")))
+  (is (= 12 (captcha-half "123123")))
+  (is (= 4  (captcha-half "12131415"))))
+  
