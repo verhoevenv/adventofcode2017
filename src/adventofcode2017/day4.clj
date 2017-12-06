e535da5284f5f5e056e20f7a4a1bed77882c2c8b
Vincent Verhoeven <verhoevenv@gmail.com>
2017-12-06 23:21:06 +0100

    Day 4 part 2

--- src/adventofcode2017/day4.clj
+++ src/adventofcode2017/day4.clj
@@ -1,9 +1,23 @@
 (ns adventofcode2017.day4)
 
-(defn valid-passphrase
+(defn words
   [phrase]
-  (apply distinct? (clojure.string/split phrase #" ")))
+  (clojure.string/split phrase #" "))
+
+(defn valid-by
+  [f phrase]
+  (every? (fn [x] (= 1 (count x))) 
+    (vals
+      (group-by f (words phrase)))))
+
+(defn valid-passphrase-duplicate
+  [phrase]
+  (valid-by identity phrase))
+
+(defn valid-passphrase-anagram
+  [phrase]
+  (valid-by (fn [x] (sort (seq x))) phrase))
 
 (defn count-valid
-  [phrases]
-  (count (filter valid-passphrase phrases)))
+  [validity phrases]
+  (count (filter validity phrases)))
--- test/adventofcode2017/day4_test.clj
+++ test/adventofcode2017/day4_test.clj
@@ -3,11 +3,26 @@
             [adventofcode2017.day4 :refer :all]))
 
 (deftest part-1
-  (is (valid-passphrase "aa bb cc dd ee"))
-  (is (not(valid-passphrase "aa bb cc dd aa")))
-  (is (valid-passphrase "aa bb cc dd aaa"))
-  (is (= 2 (count-valid [
+  (is (valid-passphrase-duplicate "aa bb cc dd ee"))
+  (is (not (valid-passphrase-duplicate "aa bb cc dd aa")))
+  (is (valid-passphrase-duplicate "aa bb cc dd aaa"))
+  (is (= 2 (count-valid valid-passphrase-duplicate [
     "aa bb cc dd ee"
     "aa bb cc dd aa"
     "aa bb cc dd aaa"
   ]))))
+
+(deftest part-2
+  (is (valid-passphrase-anagram "abcde fghij"))
+  (is (not(valid-passphrase-anagram "abcde xyz ecdab")))
+  (is (valid-passphrase-anagram "a ab abc abd abf abj"))
+  (is (valid-passphrase-anagram "iiii oiii ooii oooi oooo"))
+  (is (not (valid-passphrase-anagram "oiii ioii iioi iiio")))
+  (is (= 3 (count-valid valid-passphrase-anagram [
+    "abcde fghij"
+    "abcde xyz ecdab"
+    "a ab abc abd abf abj"
+    "iiii oiii ooii oooi oooo"
+    "oiii ioii iioi iiio"
+  ]))))
+  
